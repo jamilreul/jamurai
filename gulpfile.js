4f229838c1c95ded6a5b8efd41bf965058b40f42
jamilreul <jamil96@mail.ru>
2024-02-18 02:43:22 +0300

    start project

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+const { src, dest, watch, parallel, series } = require("gulp");
+
+const scss = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+// const { deleteAsync } = require("del");
+const browserSync = require("browser-sync").create();
+
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+    notify: false,
+  });
+}
+
+async function styles() {
+  const autoprefixer = (await import("gulp-autoprefixer")).default;
+
+  return src("app/scss/style.scss")
+    .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ["last 10 versions"], grid: true })
+    )
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
+}
+
+function scripts() {
+  return src(["node_modules/jquery/dist/jquery.js", "app/js/main.js"])
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
+}
+
+async function images() {
+  const imagemin = (await import("gulp-imagemin")).default;
+  const gifsicle = (await import("imagemin-gifsicle")).default;
+  const mozjpeg = (await import("imagemin-mozjpeg")).default;
+  const optipng = (await import("imagemin-optipng")).default;
+  const svgo = (await import("imagemin-svgo")).default;
+
+  return src("app/images/**/*.*")
+    .pipe(
+      imagemin([
+        gifsicle({ interlaced: true }),
+        mozjpeg({ quality: 75, progressive: true }),
+        optipng({ optimizationLevel: 5 }),
+        svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"));
+}
+
+function build() {
+  return src(["app/**/*.html", "app/css/style.min.css", "app/js/main.min.js"], {
+    base: "app",
+  }).pipe(dest("dist"));
+}
+
+async function cleanDist() {
+  const { deleteAsync } = await import("del");
+  return deleteAsync(["dist"]);
+}
+
+function watching() {
+  watch(["app/scss/**/*.scss"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/**/*.html"]).on("change", browserSync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.images = images;
+exports.cleanDist = cleanDist;
+exports.build = series(cleanDist, images, build);
+
+exports.default = parallel(styles, scripts, browsersync, watching);
